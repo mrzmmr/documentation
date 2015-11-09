247fac6360fc098794ceacc46322c8c175f9000c
Tom MacWright <tom@macwright.org>
2015-11-09 09:39:54 -0500

    Start --watch integration

--- bin/documentation.js
+++ bin/documentation.js
@@ -11,35 +11,44 @@ var documentation = require('../'),
   lint = require('../lib/lint'),
   args = require('../lib/args.js');
 
-var parsedArgs = args(process.argv.slice(2)),
-  formatterOptions = parsedArgs.formatterOptions,
-  outputLocation = parsedArgs.output,
-  formatter = documentation.formats[parsedArgs.formatter];
+var parsedArgs = args(process.argv.slice(2));
 
-documentation(parsedArgs.inputs, parsedArgs.options, function (err, comments) {
-  if (err) {
-    throw err;
-  }
-
-  if (parsedArgs.options.lint) {
-    var lintOutput = lint.format(comments);
-    if (lintOutput) {
-      console.log(lintOutput);
-      process.exit(1);
-    } else {
-      process.exit(0);
-    }
-  }
-
-  formatter(comments, formatterOptions, function (err, output) {
-    if (outputLocation !== 'stdout') {
-      if (parsedArgs.formatter === 'html') {
-        streamArray(output).pipe(vfs.dest(outputLocation));
-      } else {
-        fs.writeFileSync(outputLocation, output);
-      }
-    } else {
-      process.stdout.write(output);
-    }
-  });
+generate(
+  parsedArgs.inputs,
+  documentation.formats[parsedArgs.formatter],
+  parsedArgs.formatterOptions,
+  parsedArgs.options,
+  parsedArgs.output, function (err, res) {
 });
+
+function generate(inputs, formatter, formatterOptions, options, destination, callback) {
+  documentation(inputs, options, function (err, comments) {
+    if (err) {
+      throw err;
+    }
+
+    if (options.lint) {
+      var lintOutput = lint.format(comments);
+      if (lintOutput) {
+        console.log(lintOutput);
+        process.exit(1);
+      } else {
+        process.exit(0);
+      }
+    }
+
+    formatter(comments, formatterOptions, function (err, output) {
+      if (destination !== 'stdout') {
+        if (formatter === 'html') {
+          streamArray(output)
+            .pipe(vfs.dest(destination))
+            .on('end', callback);
+        } else {
+          fs.writeFile(destination, output, callback);
+        }
+      } else {
+        process.stdout.write(output);
+      }
+    });
+  });
+}
